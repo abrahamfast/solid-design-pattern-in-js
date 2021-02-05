c0839caa87e01625b16bbcac3cd9a529686b01ec
abraham ghaemi <ig.ghaemi@gmail.com>
2021-02-06 01:44:26 +0330

    add new test it

--- tests/test.js
+++ tests/test.js
@@ -1,5 +1,11 @@
 describe("Task",() => {
+
     it("has a title", () => {
         expect(new Task().title).toEqual("Untitled Task")
     })
+
+    it("can be set a title", () => {
+        expect(new Task({title: "My First Task"}).title).toEqual("My First Task")
+    })
+    
 })
